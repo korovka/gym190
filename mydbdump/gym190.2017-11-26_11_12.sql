e779b6003ec20c5084d32738e877159e0211a3bd
korovka <ms.irina.belyaeva@gmail.com>
2017-11-26 11:16:47 +0300

    collate regru --> general

--- mydbdump/gym190.2017-11-26_11_12.sql
+++ mydbdump/gym190.2017-11-26_11_12.sql
@@ -32,10 +32,10 @@ DROP TABLE IF EXISTS `migration`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `migration` (
-  `version` varchar(180) COLLATE utf8_regru_ci NOT NULL,
+  `version` varchar(180) COLLATE utf8_general_ci NOT NULL,
   `apply_time` int(11) DEFAULT NULL,
   PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_regru_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
